1934601b1485d0865ffd6c8e5e71bc85c77d7670
Pedro Lima <peulima2004@gmail.com>
2025-05-03 01:33:23 +0000

    add register endpoint

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import db from '../database/configdb.js';
+import userRoutes from '../routes/user.routes.js';
 import User from '../models/user.js';
 
 dotenv.config();
@@ -10,6 +11,8 @@ db.connectDB();
 const app = express();
 
 app.use(express.json());
+
+app.use("/users", userRoutes);
 
 app.get('/', (req, res) => {
     res.send({message: 'hello world'});
--- models/user.js
+++ models/user.js
@@ -4,10 +4,12 @@ const configSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true, // Ensure username is unique
     },
     password: {
         type: String,
         required: true,
+        select: false, // Do not include password in queries by default
     },
 });
 
